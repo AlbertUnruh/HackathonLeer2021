7aa9fe76b48c135c02baa3577e524d7288c965b8
Albert Unruh <keenan.noack@gmail.com>
2021-05-17 19:55:52 +0200

    better `TOKEN`

--- CONFIGS.py
+++ CONFIGS.py
@@ -1,7 +1,12 @@
+from os import environ
 
 
 PREFIX = "h!"
 
 # can be stored where ever you want :)
-with open("TOKEN") as f:
-    TOKEN = f.read()
+# at first the code checks if the is a environ variable,
+# otherwise it read the token from a file
+TOKEN = environ.get("DISCORD_BOT_TOKEN", None)
+if TOKEN is None:
+    with open("TOKEN") as f:
+        TOKEN = f.read()
