609c1b1ee43d0c9a40303b500f83da41d9d22a65
Albert Unruh <keenan.noack@gmail.com>
2021-05-19 20:51:17 +0200

    PEP8 and other PEPs

--- modules/role.py
+++ modules/role.py
@@ -5,6 +5,7 @@ from discord.utils import get
 from typing import Optional
 from contributor import MikeCodes2586
 
+
 class RoleCog(Cog, name="RoleManager"):
     """is a cog with funtions for creating, deleting and destributing roles"""
 
@@ -14,7 +15,7 @@ class RoleCog(Cog, name="RoleManager"):
         self.bot = bot
 
     @command(name="make_team")
-    async def create_team(self, ctx: Context, *name: str) -> Role:
+    async def create_team(self, ctx: Context, *name: str):
         """creates a role, category, a text and a voice channel for a team and gives the author the role"""
         team_name = " ".join(name)
         team_role: Role = await self.bot.guilds[0].create_role(name=team_name)
@@ -23,28 +24,35 @@ class RoleCog(Cog, name="RoleManager"):
 
         t_cat_overwrites = {
             self.bot.guilds[0].default_role: PermissionOverwrite(**{
-                "view_channel" : False
+                "view_channel": False
             }),
             team_role: PermissionOverwrite(**{
-                "view_channel" : True
+                "view_channel": True
             })
         }
-        team_category: CategoryChannel = await self.bot.guilds[0].create_category(name=team_name, overwrites=t_cat_overwrites)
+        team_category: CategoryChannel = await self.bot.guilds[0].create_category(
+            name=team_name, overwrites=t_cat_overwrites)
 
         t_cha_overwrites = {
             self.bot.guilds[0].default_role: PermissionOverwrite(**{
-                "view_channel" : False
+                "view_channel": False
             }),
             team_role: PermissionOverwrite(**{
-                "view_channel" : True
+                "view_channel": True
             })
         }
-        await self.bot.guilds[0].create_text_channel(name="Text Channel", overwrites=t_cha_overwrites, category=team_category)
+        await self.bot.guilds[0].create_text_channel(name="Text Channel",
+                                                     overwrites=t_cha_overwrites,
+                                                     category=team_category)
 
-        await self.bot.guilds[0].create_voice_channel(name="Voice Channel", overwrites=t_cha_overwrites, category=team_category)
+        await self.bot.guilds[0].create_voice_channel(name="Voice Channel",
+                                                      overwrites=t_cha_overwrites,
+                                                      category=team_category)
 
     @command(name="remove_team")
-    async def remove_category(self, ctx: Context, category: Optional[CategoryChannel] = None):
+    async def remove_team(self, ctx: Context,
+                          category: Optional[CategoryChannel] = None):
+        """deletes a category with the channels inside"""
         if category is None:
             return
 
