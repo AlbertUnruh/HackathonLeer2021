db59e46771006c91baf446756a98c28bfbe2659b
Albert Unruh <keenan.noack@gmail.com>
2021-05-21 15:53:19 +0200

    users can now login (TODO: check if they are already logged in)

---
+++ database.py
@@ -0,0 +1,102 @@
+"""this file contains the DataBase manager"""
+from sqlite3 import connect, Connection, Cursor
+from typing import Union, List
+from contributor import AlbertUnruh
+
+
+__all__ = (
+    "DataBase",
+    "User",
+)
+
+TABLE_USER = "user"
+
+
+class DataBase:
+    """is the DataBase class"""
+
+    contributor = [AlbertUnruh]
+
+    def __init__(self, *, name: str = "database.sqlite"):
+        self._connection: Connection = connect(name)
+        self._cursor: Cursor = self._connection.cursor()
+
+    def commit(self):
+        """commits the statements to the DataBase"""
+        self._connection.commit()
+
+    def execute(self, cmd: Union[str, List[str]], *, autocommit=True):
+        """executes a command"""
+
+        if isinstance(cmd, str):
+            cmd = [cmd]
+
+        for raw in cmd:
+            for c in raw.split(";"):
+                self._cursor.execute(c)
+
+        if autocommit:
+            self.commit()
+
+    def get_all(self, table, **query):
+        """fetches all entries from a table with the given query"""
+        name, query = query.popitem()
+        self._cursor.execute(f"""
+SELECT * FROM {table} WHERE {name}=={query!r}
+""")
+        return self._cursor.fetchall()
+
+    def close(self):
+        """closes the DB"""
+        self._connection.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+
+class User:
+    """is a special class for the User from the DataBase"""
+
+    contributor = [AlbertUnruh]
+
+    # for PyCharm
+    # noinspection PyShadowingBuiltins
+    @staticmethod
+    def new_user(*, id: int, user: str, name: str, mail: str,
+                 school: str, cl4ss: str, team: str):
+        """adds a User to the DataBase"""
+        with DataBase() as db:
+            db.execute(f"""\
+INSERT INTO {TABLE_USER!r} VALUES (
+    {id!r},
+    {user!r},
+    {mail!r},
+    {name!r},
+    {school!r},
+    {cl4ss!r},
+    {team!r}
+);
+""")
+
+    @staticmethod
+    def get_users(**query) -> list:
+        """gets users by the query"""
+        assert len(query.items()) == 1, "Please insert only ONE query!"
+        with DataBase() as db:
+            return db.get_all(TABLE_USER, **query)
+
+    with DataBase() as db:
+        db.execute(f"""\
+CREATE TABLE IF NOT EXISTS {TABLE_USER!r} (
+    'id'        INTEGER,
+    'user'      TEXT,
+    'mail'      TEXT,
+    'name'      TEXT,
+    'school'    TEXT,
+    'cl4ss'     TEXT,
+    'team'      TEXT
+);
+""")
