5287999b018b481d69c9bf4e4f8e84d2f79b1e8e
Albert Unruh <keenan.noack@gmail.com>
2021-05-24 14:27:25 +0200

    preventing SQL-injections and PEP-8

--- modules/role.py
+++ modules/role.py
@@ -6,6 +6,11 @@ from typing import Optional
 from contributor import MikeCodes2586
 from CONFIGS import PREFIX
 from database import User
+from re import compile
+
+
+VALID_CHARS = compile(r"[a-zA-Z0-9_.+\- @äöü]")
+
 
 class RoleCog(Cog, name="RoleManager"):
     """is a cog with funtions for creating, deleting and destributing roles"""
@@ -23,7 +28,8 @@ class RoleCog(Cog, name="RoleManager"):
         user = User.get_users(id=ctx.author.id)[0]
         if user[6] != team_name:
             embed: Embed = Embed(color=0x5865F2, title="Fehler")
-            embed.add_field(name= "Nicht in Team", value= f"Melde dich in einem DM-Channel mit `{self.bot.user}` mit `{PREFIX}anmelden` an")
+            embed.add_field(name="Nicht in Team",
+                            value=f"Melde dich in einem DM-Channel mit `{self.bot.user}` mit `{PREFIX}anmelden` an")
             await ctx.channel.send(embed=embed)
             return
 
@@ -39,7 +45,8 @@ class RoleCog(Cog, name="RoleManager"):
                 "view_channel": True
             })
         }
-        team_category: CategoryChannel = await self.bot.guilds[0].create_category(
+        team_category: CategoryChannel = await self.bot.guilds[
+            0].create_category(
             name=team_name, overwrites=t_cat_overwrites)
 
         t_cha_overwrites = {
@@ -63,13 +70,15 @@ class RoleCog(Cog, name="RoleManager"):
                           category: Optional[CategoryChannel] = None):
         """deletes a category with the channels inside"""
         if category is None:
-            await ctx.channel.send(f"Bitte gebe einen richtigen Teamnamen an! \nAchte auf Großschreibung \nNutzung: `{PREFIX}remove_team [dein team name]` (in \" \" wenn er Leerzeichen hat)")
+            await ctx.channel.send(
+                f"Bitte gebe einen richtigen Teamnamen an! \nAchte auf Großschreibung \nNutzung: `{PREFIX}remove_team [dein team name]` (in \" \" wenn er Leerzeichen hat)")
             return
 
         user = User.get_users(id=ctx.author.id)[0]
         if user[6] != category.name:
             embed: Embed = Embed(color=0x5865F2, title="Fehler")
-            embed.add_field(name= "Nicht in Team", value= f"Melde dich in einem DM-Channel mit `{self.bot.user}` mit `{PREFIX}anmelden` an")
+            embed.add_field(name="Nicht in Team",
+                            value=f"Melde dich in einem DM-Channel mit `{self.bot.user}` mit `{PREFIX}anmelden` an")
             await ctx.channel.send(embed=embed)
             return
 
@@ -84,13 +93,15 @@ class RoleCog(Cog, name="RoleManager"):
         await category.delete()
 
     @command(name="join_team")
-    async def give_author_role(self, ctx: Context, role: Optional[Role] = None):
+    async def give_author_role(self, ctx: Context,
+                               role: Optional[Role] = None):
         """gives the author the selected role"""
 
         user = User.get_users(id=ctx.author.id)[0]
         if user[6] != role.name:
             embed: Embed = Embed(color=0x5865F2, title="Fehler")
-            embed.add_field(name= "Nicht in Team", value= f"Melde dich in einem DM-Channel mit `{self.bot.user}` mit `{PREFIX}anmelden` an")
+            embed.add_field(name="Nicht in Team",
+                            value=f"Melde dich in einem DM-Channel mit `{self.bot.user}` mit `{PREFIX}anmelden` an")
             await ctx.channel.send(embed=embed)
             return
 
@@ -99,13 +110,15 @@ class RoleCog(Cog, name="RoleManager"):
         if role in ctx.author.roles:
             await ctx.channel.send("Es hat funtioniert!")
         elif role not in ctx.author.roles:
-            await ctx.channel.send("Etwas ist falsch gelaufen und du hast die Rolle nicht bekommen. \nVersuchs nochmal!")
+            await ctx.channel.send(
+                "Etwas ist falsch gelaufen und du hast die Rolle nicht bekommen. \nVersuchs nochmal!")
         else:
-            await ctx.channel.send("Irgendwas ist seehr falsch gelaufen. Bitte kontaktiere einen Dev.")
+            await ctx.channel.send(
+                "Irgendwas ist seehr falsch gelaufen. Bitte kontaktiere einen Dev.")
 
     @command(name="exit_team")
     async def remove_author_role(self, ctx: Context,
-                              role: Optional[Role] = None):
+                                 role: Optional[Role] = None):
         """gives the author the selected role"""
 
         await self.bot.guilds[0].get_member(ctx.author.id).remove_roles(role)
@@ -113,20 +126,23 @@ class RoleCog(Cog, name="RoleManager"):
         if role not in ctx.author.roles:
             await ctx.channel.send("Es hat funtioniert!")
         elif role in ctx.author.roles:
-            await ctx.channel.send("Etwas ist falsch gelaufen und du hast die Rolle immer noch. \nVersuchs nochmal!")
+            await ctx.channel.send(
+                "Etwas ist falsch gelaufen und du hast die Rolle immer noch. \nVersuchs nochmal!")
         else:
-            await ctx.channel.send("Irgendwas ist seehr falsch gelaufen. Bitte kontaktiere einen Dev.")
+            await ctx.channel.send(
+                "Irgendwas ist seehr falsch gelaufen. Bitte kontaktiere einen Dev.")
 
     @command(name="check_team")
-    async def check_for_team_in_DB(self, ctx: Context,
-                                  *name: str):
+    async def check_for_team_in_db(self, ctx: Context,
+                                   *name: str):
         """checks if the given team is in the database"""
 
         team_name = " ".join(name)
-        team = User.get_users(team=team_name)
+        team = User.get_users(team="".join(VALID_CHARS.findall(team_name)))
 
         if not team:
-            await ctx.channel.send(f"Das Team gibt es nicht (achte auf Großschreibung). \nDu kannst ein Team mit `{PREFIX}make_team mein teamname`(Leerzeichen möglich) erstellen wenn du die `manage_roles` permission hast.")
+            await ctx.channel.send(
+                f"Das Team gibt es nicht (achte auf Großschreibung). \nDu kannst ein Team mit `{PREFIX}make_team mein teamname`(Leerzeichen möglich) erstellen wenn du die `manage_roles` permission hast.")
             return
-        await ctx.channel.send(f"Das Team existiert und hat {len(team)} Mitglieder")
-
+        await ctx.channel.send(
+            f"Das Team existiert und hat {len(team)} Mitglieder")
