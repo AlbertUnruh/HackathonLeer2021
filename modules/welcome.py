6f6c4578aa4e82c48e16ddf895ccc8f0d29d51e3
Albert Unruh <keenan.noack@gmail.com>
2021-05-25 10:48:16 +0200

    now sending a Goodbye message to

--- modules/welcome.py
+++ modules/welcome.py
@@ -2,13 +2,13 @@
 from discord.ext.commands import Bot, Cog
 from discord import TextChannel, Member, PermissionOverwrite
 from json import load, dump
-from contributor import MikeCodes2586
+from contributor import MikeCodes2586, AlbertUnruh
 
 
 class WelcomeCog(Cog, name="Welcome"):
     """is a Cog for welcoming new users"""
 
-    contributor = [MikeCodes2586]
+    contributor = [MikeCodes2586, AlbertUnruh]
 
     def __init__(self, bot: Bot):
         self.bot = bot
@@ -16,21 +16,29 @@ class WelcomeCog(Cog, name="Welcome"):
     @Cog.listener("on_member_join")
     async def send_welcome_msg(self, member: Member):
         """Sends welcome messages on join event"""
-        channel_id = self.get_channel_ids_json().get("WELCOME", None)
-        if channel_id is None:
-            channel = await self.create_channel_welcome()
-        else:
-            channel: TextChannel = self.bot.guilds[0].get_channel(channel_id)
-            if channel is None:
-                channel = await self.create_channel_welcome()
+        await (await self.get_channel()).send(f'Wilkommen auf dem Server {member.mention}!')
 
-        await channel.send(f'Wilkommen auf dem Server {member.mention}!')
+    @Cog.listener("on_member_remove")
+    async def send_goodbye_msg(self, member: Member):
+        """Sends welcome messages on remove event"""
+        await (await self.get_channel()).send(f'Bis bald {member.mention}!')
 
     @staticmethod
     def get_channel_ids_json() -> dict:
         """returns the ids from the json-file"""
         with open("./CHANNEL_IDS.json") as f:
             return load(f)
+
+    async def get_channel(self) -> TextChannel:
+        """gets the welcome channel"""
+        channel_id = self.get_channel_ids_json().get("WELCOME", None)
+        if channel_id is None:
+            return await self.create_channel_welcome()
+        else:
+            channel: TextChannel = self.bot.guilds[0].get_channel(channel_id)
+            if channel is None:
+                return await self.create_channel_welcome()
+        return channel
 
     async def create_channel_welcome(self) -> TextChannel:
         """creates a new channel for welcome messages"""
