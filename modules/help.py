004bfc0a7b39d87d1f0a2c11f69dd0c70e69c355
Albert Unruh <keenan.noack@gmail.com>
2021-05-25 14:46:55 +0200

    updated the help

--- modules/help.py
+++ modules/help.py
@@ -1,20 +1,56 @@
+"""contains the Cog for the Help cmd"""
 from discord.ext.commands import Bot, Cog, command, Context
 from contributor import Nxnx0502
 from discord import Embed
+import CONFIGS
+
 
 class HelpCog(Cog, name="Help"):
-    contributor=[Nxnx0502]
+    """is the COg for the Help"""
+    contributor = [Nxnx0502]
 
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @command(name="help", aliases=["Help"])
+    @command(name="help", aliases=["h"])
     async def helpcommand(self, ctx: Context):
-        embed: Embed = Embed(color=0x5865F2, title="Hilfe")
-        embed.add_field(name="Suche", value="Du brauchst Hilfe? Dann wähle eine Suchmaschine aus: \n> -ddg | duckduckgo | browse | search | s \n> -gh | github \n> -g | google \n> -sof | stackoverflow \n> -ecosia \n> -bing", inline=True)
-        embed.add_field(name="Teams", value="So erstellst du ein neues Team: \n> make_team Teamname \n> (Der Teamname darf auch Leerzeichen enthalten) \n \n So löscht du ein Team: \n> remove_team Teamname \n \n Wenn es dein Team schon gibt kannst du so joinen: \n> join_team Teamname \n \n Dein Team nervt? Dann verlasse dein Team einfach: \n> exit_team Teamname", inline=True)
-        embed.add_field(name="Anmelden", value='Um dich anzumelden schreibe dem Bot im Privatchat: \n> anmelden "E-Mail" "Name" "Schule" "Klasse" "Teamname"', inline=False)
-        embed.add_field(name="Latenz", value="Hier sind die Latenzen zusehen: \n> :ping_pong:", inline=True)
-        embed.add_field(name="Mitwirkende", value="Du willst wissen wer bei diesem Bot Mist gebaut hat? So kriegst du die Namen für die Beschwerde raus: \n> contributer", inline=True)
+        """displays the help"""
+        embed: Embed = Embed(color=0x5865F2, title="Hilfe",
+                             description=f"Mein Präfix ist `{CONFIGS.PREFIX}`")
+
+        embed.add_field(name="Suche", value="""\
+Du brauchst Hilfe? Dann wähle eine Suchmaschine aus:
+> -ddg | duckduckgo | browse | search | s
+> -gh | github
+> -g | google
+> -sof | stackoverflow
+> -ecosia
+> -bing""")
+
+        embed.add_field(name="Teams", value="""\
+So erstellst du ein neues Team:
+> make_team Teamname
+> (Der Teamname darf auch Leerzeichen enthalten)
+
+So löscht du ein Team:
+> remove_team Teamname
+
+Wenn es dein Team schon gibt kannst du so joinen:
+> join_team Teamname
+
+Dein Team nervt? Dann verlasse dein Team einfach:
+> exit_team Teamname""")
+
+        embed.add_field(name="Anmelden", value="""
+Um dich anzumelden schreibe dem Bot im Privatchat:
+> anmelden "E-Mail" "Name" "Schule" "Klasse" "Teamname" """, inline=False)
+
+        embed.add_field(name="Latenz", value="""\
+Hier sind die Latenzen zusehen:
+> :ping_pong:""")
+
+        embed.add_field(name="Mitwirkende", value="""
+Du willst wissen wer bei diesem Bot Mist gebaut hat? So kriegst du die Namen für die Beschwerde raus:
+> contributer""")
 
         await ctx.channel.send(embed=embed)
