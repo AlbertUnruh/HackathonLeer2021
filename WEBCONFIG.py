55303ed3ea593e7ee8fd2a47093c8ab3e8d2e945
RedstoneCraft <65400532+RedstoneCraft@users.noreply.github.com>
2021-05-27 17:59:54 +0200

    Update WEBCONFIG.py

--- WEBCONFIG.py
+++ WEBCONFIG.py
@@ -1,21 +1,7 @@
 import CONFIGS
 from os import environ
 
-SECRET_KEY = environ.get("SECRET_KEY", None)
-if SECRET_KEY is None:
-    with open("SECRET_KEY") as f:
-        SECRET_KEY = f.read()
-
-DISCORD_CLIENT_SECRET = environ.get("DISCORD_CLIENT_SECRET", None)
-if DISCORD_CLIENT_SECRET is None:
-    with open("DISCORD_CLIENT_SECRET") as f:
-        DISCORD_CLIENT_SECRET = f.read()
-
-DISCORD_REDIRECT_URI = environ.get("DISCORD_REDIRECT_URI", None)
-if DISCORD_REDIRECT_URI is None:
-    with open("DISCORD_REDIRECT_URI") as f:
-        DISCORD_REDIRECT_URI = f.read()
-
-DISCORD_CLIENT_ID = 843893717907406898
-
-DISCORD_BOT_TOKEN = CONFIGS.TOKEN
+PASSWORD = environ.get("PASSWORD", None)
+if PASSWORD is None:
+    with open("PASSWORD") as pass:
+        PASSWORD = pass.read()
