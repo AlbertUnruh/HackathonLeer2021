7593df2c4e9d277ef51abf1f2dcbb90492cb935f
RedstoneCraft <65400532+RedstoneCraft@users.noreply.github.com>
2021-05-27 15:57:37 +0200

    Update web-dashboard.py

--- web-dashboard.py
+++ web-dashboard.py
@@ -2,9 +2,13 @@ from quart import Quart, render_template, request, session, redirect, url_for
 from quart_discord import DiscordOAuth2Session
 from discord.ext import ipc
 import WEBCONFIG
-from database import DbUser
+from database import User
+from modules.countdown import CountdownCog
+from contributor import RedstoneCraft
+
+contributor = [RedstoneCraft]
 ### from datenbank-utils import sqlite-kram
-app = Quart(__name__, template_folder="templates", )
+app = Quart(__name__, template_folder="templates")
 ipc_client = ipc.Client(secret_key=WEBCONFIG.SECRET_KEY)
 
 app.config["DISCORD_CLIENT_ID"] = WEBCONFIG.DISCORD_CLIENT_ID
@@ -18,7 +22,8 @@ discord = DiscordOAuth2Session(app)
 
 @app.route("/")
 async def home():
-    await render_template("index.html")
+    users = User.get_all_users()
+    await render_template("index.html", user = users)
 
 @app.route("/login/")
 async def login():
@@ -29,18 +34,10 @@ async def dashboard():
     authorized = await discord.authorized
     if authorized is not True:
         return redirect(url_for("login"))
-    
+    termins = await CountdownCog().get_timestamps()
     user = await discord.fetch_user()
     user_name = ipc_client.request("get_display_name_by_id", user_id=user["user_id"])
     #anmeldungs_formular = sqlite utils get_anmeldungen()
-    
-#################### API ####################
+    return render_template("dashboard.html", termins = termins )
 
-@app.route("/api/anmeldungen/")
-async def api_anmeldungen():
-    return DbUser.get_all_users()
-
-@app.route("/api/termine/")
-async def api_termine():
-    ##### albert hier termine auslesen discord channel
-    return termine
+app.run(host = 1234, port = 1234)
