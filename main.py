5ad90c2214693429f33847d465c7fd597d0df868
Albert Unruh <keenan.noack@gmail.com>
2021-05-27 17:48:09 +0200

    main is now again only for the bot

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ from modules import register_commands
 from CONFIGS import TOKEN, PREFIX
 from contributor import print_contributor
 
-from threading import Thread
-from asyncio import run_coroutine_threadsafe, get_event_loop
-from web_dashboard import run
-
 
 intents = Intents.all()
 
@@ -21,7 +17,6 @@ intents = Intents.all()
 bot = Bot(PREFIX, None, intents=intents, case_insensitive=True)
 register_commands(bot)
 
-run_coroutine_threadsafe(run(bot), get_event_loop())
 
 print_contributor(bot)
 bot.run(TOKEN)
